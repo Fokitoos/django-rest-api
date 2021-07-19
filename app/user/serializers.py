fe0f98324e16596e079a47b11f1b3d56539ed26e
ifok <john.fokianos@gmail.com>
2021-07-19 21:44:13 +0200

    added user endpoints and unit tests

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import get_user_model
-
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 
@@ -16,4 +16,40 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
 
         return get_user_model().objects.create_user(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """ A user setting the password correctly and return it """
+        password = validated_data.pop('password',None)
+        user = super().update(instance, validated_data)
 
+        if password:
+            user.set_password(password)
+            user.save()
+        
+        return user
+
+class AuthTokenSerializer(serializers.Serializer):
+    """ Serialiazer for the user authontication object """
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type' : 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """ Validate and authenticate the user """
+        email =attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+
+        if not user:
+            msg = ('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+        
+        attrs['user'] = user 
+        return attrs
